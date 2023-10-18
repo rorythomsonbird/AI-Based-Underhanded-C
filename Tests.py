c7c74cee02272153d6192a1b8a795e1c7bcf74ac
ZJAC103 <zjac103@live.rhul.ac.uk>
2023-10-18 16:26:43 +0100

    Fix problem where test 1 was case sensitive and stop the API from coding a c program to say hello

--- Tests.py
+++ Tests.py
@@ -3,12 +3,18 @@ import MalwareCheck
 import RandomCGen
 def apitest():
     print("GPTAPI TESTS")
-    if "hello" in GPTAPI.generate_response("Say hello"): #Test 1, API response should include the word hello
+
+    #Test 1, API response should include the word hello
+    test1str = GPTAPI.generate_response("Say hello, dont code it.")
+    if "hello"or"Hello" in test1str: 
         print("TEST 1 SUCCESS")
     else:
         print("TEST 1 FAIL")
 
-    if "```c" in GPTAPI.generate_response("Write a simple c code snippet"):
+
+    #Test 2, API response should include the beginning of a c snippet
+    test2str = GPTAPI.generate_response("Write a simple c code snippet")
+    if "```c" in test2str:
         print("TEST 2 SUCCESS")
     else:
         print("TEST 2 FAIL")
