fa1789ee333c5ca36bd9dd9de8abb7db3eb652bb
ZJAC103 <zjac103@live.rhul.ac.uk>
2023-10-19 18:51:05 +0100

    Create GUI class

--- Samples/sample13.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-
-int main() {
-    int num = 10;
-    printf("The value of num is: %d\n", num);
-    return 0;
-}
-
--- Tests.py
+++ Tests.py
@@ -1,6 +1,7 @@
 import GPTAPI
 import MalwareCheck
 import RandomCGen
+import os
 def apitest():
     print("GPTAPI TESTS")
 
@@ -18,4 +19,29 @@ def apitest():
         print("TEST 2 SUCCESS")
     else:
         print("TEST 2 FAIL")
+
+def cgentest():
+    print("RANDOMCGEN TESTS")
+
+    #Test1, file count should grow
+    dir_path = r'Samples'
+    fcount = 0
+    for path in os.listdir(dir_path): #count current numbers of files in directory
+        
+        fcount += 1
+    #blockPrint()
+    RandomCGen.gencode(1,"")
+    #enablePrint()
+    newfcount = 0
+    for path in os.listdir(dir_path): #count current numbers of files in directory
+        newfcount += 1
+    if newfcount>fcount:
+        print("TEST 1 SUCCESS")
+    else:
+        print("TEST 1 FAIL")
+
+
+
+
 apitest()
+cgentest()
