4da670d536f9f6604a968b487fbe2a183c6d2af6
ZJAC103 <zjac103@live.rhul.ac.uk>
2023-10-25 15:22:55 +0100

    Add functionality for GUI of generating random code based on number of wanted files

--- GUI.py
+++ GUI.py
@@ -1,6 +1,13 @@
 import tkinter
+import RandomCGen
 gui = tkinter.Tk()
 gui.title("Malware Generator")
+
+def sendprompt(): 
+    input = insertinp.get("1.0",'end-1c') #1.0 refers to the first line character zero (line.character), end-1c means read to end of text then remove unwanted newline char
+    if input != "":
+        RandomCGen.gencode(int(input), "")
+
 frame = tkinter.Frame(gui, width=800,height=500)
 frame.pack_propagate(0)
 frame.pack()
@@ -9,7 +16,7 @@ insertlab.config(font=("Courier", 8))
 insertlab.place(x=5,y=100)
 insertinp = tkinter.Text(frame, height= 1, width=20)
 insertinp.place(x=10, y=120)
-button = tkinter.Button(frame, text = "Generate!",width = 25, command=gui.destroy)
+button = tkinter.Button(frame, text = "Generate!",width = 25, command=sendprompt)
 button.place(x=300,y= 480)
 
 
---
+++ Samples/sample8.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int result = num1 + num2;
+
+    printf("The result is: %d\n", result);
+
+    return 0;
+}
+
