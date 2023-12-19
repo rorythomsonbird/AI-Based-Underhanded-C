799941e4072a2e567d81a446c5a4d4a3a70d454c
ZJAC103 <zjac103@live.rhul.ac.uk>
2023-12-19 17:58:54 +0000

    Add location of the prompt TextBox and Button class

--- WORMAPI.py
+++ WORMAPI.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
 def api():
-    url = 'https://flowgpt.com/p/wormgpt-6'
-    
+    url = 'https://flowgpt.com/p/wormgpt-6' #WormGPT URL
+    textbox_id = 'chat-input-textarea'  #ID for prompt textbox
+    button_class = 'absolute z-50 rounded-md p-1 text-gray-500 right-1 disabled:opacity-40 enabled:hover:text-gray-400 disabled:hover:bg-transparent md:right-2'  #Send prompt button class
 
 if __name__ == '__main__':
     
