8110a2e4dc068a5ce9d025e7c27c01b809e02b69
ZJAC103 <zjac103@live.rhul.ac.uk>
2024-01-21 21:18:23 +0000

    Circumvent focus guard in WORMAPI

--- WORMAPI.py
+++ WORMAPI.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class WORMAPI:
     def send_message():
@@ -17,12 +20,14 @@ class WORMAPI:
         time.sleep(3)
         signin = driver.find_element(By.ID, 'menu-button-:r15:')
         signin.click()
-        time.sleep(100)
-        google = driver.find_element(By.XPATH, '//button[@aria-label="google"]')
-        google.click()
-        time.sleep(3)
-        email = driver.find_element(By.ID, 'identifierId')
-        email.send_keys("wormywormman2@gmail.com")
+        time.sleep(10)
+        #Get around the focus guard
+        guarddelete = driver.find_element(By.CSS_SELECTOR,"div[data-focus-guard='true'][tabindex='0'][style='width: 1px; height: 0px; padding: 0px; overflow: hidden; position: fixed; top: 1px; left: 1px;'][data-aria-hidden='true'][aria-hidden='true']")
+        driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", guarddelete)
+        guarddelete = driver.find_element(By.CSS_SELECTOR,"div.chakra-portal[data-aria-hidden='true'][aria-hidden='true']")
+        driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", guarddelete)
+        #email = 
+        #email.send_keys("wormywormman2@gmail.com")
         time.sleep(30)
         textarea = driver.find_element('css selector', 'textarea[data-testid="chat-input-textarea"]')
         button = driver.find_element('css selector', 'button[aria-label="Send"]')
@@ -30,6 +35,10 @@ class WORMAPI:
         textarea.send_keys("Hello")
         button.click()
         time.sleep(300)
+    
+    #def gmaillogin():
+
+
         
         
       
